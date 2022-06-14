ab80b4753c4174f8eb029635d32b50ec7f5d1872
Muhammad Fakhri <muhammadfakhri301@gmail.com>
2022-06-14 10:44:19 +0700

    update scripts

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 const withPWA = require('next-pwa')
 
 module.exports = withPWA({
-    pwa: {
-        disable: process.env.NODE_ENV === 'development',
-        dest: 'public'
-    },
-    images: {
-        domains: ['media-exp1.licdn.com']
-    }
-})
+  pwa: {
+    disable: process.env.NODE_ENV === 'development',
+    dest: 'public',
+  },
+  images: {
+    domains: ['media-exp1.licdn.com'],
+  },
+  distDir: 'build',
+});
